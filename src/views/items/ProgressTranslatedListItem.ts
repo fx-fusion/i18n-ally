962be1af7a705bfd95d9854725b405f2f9084710
mac-pro <mac-pro@root.com>
2026-07-01 02:56:52 +0800

    c

--- src/views/items/ProgressTranslatedListItem.ts
+++ src/views/items/ProgressTranslatedListItem.ts
@@ -1,9 +1,18 @@
 import { ProgressSubmenuItem } from './ProgressSubmenuItem'
 import { ProgressRootItem } from './ProgressRootItem'
+import { Config } from '~/core';
 
 export class ProgressTranslatedListItem extends ProgressSubmenuItem {
   constructor(protected root: ProgressRootItem) {
     super(root, 'view.progress_submenu.translated_keys', 'checkmark')
+  }
+
+  // @ts-expect-error
+  get contextValue() {
+    const values: string[] = []
+    if (this.node.locale !== Config.sourceLanguage)
+      values.push('translatable')
+    return values.join('-')
   }
 
   getKeys() {
